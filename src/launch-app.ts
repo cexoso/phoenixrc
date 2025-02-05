2fa29020445b7e70e867a65084e3ec600e715327
xiongjie <jiexiong@futunn.com>
2025-02-05 20:34:19 +0800

    支持多窗口切换

--- src/launch-app.ts
+++ src/launch-app.ts
@@ -1,3 +1,22 @@
+let lastWindow: Window | undefined = undefined
+let timerHandler: number | undefined = undefined
+
+function getNextWindow(windows: Window[]): Window | undefined {
+  const lastWindows = windows.findIndex((w) => lastWindow !== undefined && w.isEqual(lastWindow))
+  if (windows.length === 0) {
+    return undefined
+  }
+  if (lastWindows === -1) {
+    return windows[0]
+  }
+
+  const next = lastWindows + 1
+  if (next >= windows.length) {
+    return windows[0]
+  }
+
+  return windows[next]
+}
 
 // appName 是应用在 macOS 上的名称，可以使用 ls /Applications 来查看目前 mac 上存在的应用
 // 你会看到类型 Google Chrome.app 格式的目录名，如果你希望唤起这个应用, 可以使用
@@ -5,7 +24,24 @@
 function focusOrLaunch(appNameOrBundleId: string) {
   const app = App.get(appNameOrBundleId)
   if (app) {
-    app.focus()
+    if (timerHandler) {
+      Timer.off(timerHandler)
+    }
+    const windows = app?.windows()
+    const window = getNextWindow(windows)
+
+    if (window === undefined) {
+      // 存在应用被打开又没有窗口的场景，这种场景需要使用 app.focus 来唤起应用
+      app.focus()
+      return
+    }
+    lastWindow = window
+
+    timerHandler = Timer.after(2, () => {
+      lastWindow = undefined
+    })
+
+    window.focus()
   } else {
     App.launch(appNameOrBundleId, { focus: true })
   }
